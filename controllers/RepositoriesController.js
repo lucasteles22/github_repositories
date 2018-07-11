16767b5b4f00a47c7d49fe4828d448da5a834c7e
Lucas Teles <lucasteles22@gmail.com>
2018-07-11 13:43:06 -0300

    Implements fetcher respositories at RepositoriesController

--- controllers/RepositoriesController.js
+++ controllers/RepositoriesController.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const GithubGraphQLApi = require('node-github-graphql');
 const github = new GithubGraphQLApi({
   token: process.env.GITHUB_ACCESS_TOKEN,
@@ -31,13 +32,22 @@ query($start: Int, $after: String, $login: String = ""){
 `;
 
 const Index = async (_, res) => {
-  fetch(res); 
+  await fetch(res);
 };
 
-function fetch(fn, array = [], start = 10, after = null, login = "thoughtworks"){
-  github.query(query, { start: start, after: after, login: login }, (req, res) => {
+async function fetch(fn, array = [], start = 10, after = null, login = "thoughtworks"){
+  github.query(query, { start: start, after: after, login: login }, async (req, res) => {
     const repositories = req.data.organization.repositories;
+    for(let element of repositories.edges) {
+      const host = "https://api.github.com/repos";
+      const clientId = process.env.GITHUB_CLIENT_ID;
+      const clientSecret = process.env.GITHUB_CLIENT_SECRET;
+      const url = `${ host }${ element.node.resourcePath }/contributors?client_id=${ clientId }&client_secret=${ clientSecret }`;
+      const collaborators = await fetchCollaborators(url);
+      element.collaborators = collaborators;
+    }
     array = array.concat(repositories.edges);
+
     if(repositories.pageInfo.hasNextPage) {
       const endCursor = repositories.pageInfo.endCursor;
       fetch(fn, array, start, endCursor, login);
@@ -46,5 +56,31 @@ function fetch(fn, array = [], start = 10, after = null, login = "thoughtworks")
     }
   });
 };
+
+function collaborators(element) {
+  const host = "https://api.github.com/repos";
+  const clientId = process.env.GITHUB_CLIENT_ID;
+  const clientSecret = process.env.GITHUB_CLIENT_SECRET;
+  const url = `${ host }${ element.node.resourcePath }/contributors?client_id=${ clientId }&client_secret=${ clientSecret }`;
+  collaborators = fetchCollaborators(url);
+  element.collaborators = collaborators;
+  return element;
+}
+
+async function fetchCollaborators(url, collaborators = []) {
+  try {
+    const response = await axios.get(url);
+    collaborators = collaborators.concat(response.data);
+    const link = response.headers.link;
+    // TODO alterar esse método horrível
+    if(link && link.split(",")[0].match("next")) {
+      const nextPageUrl = link.split(",")[0].split(";")[0].replace(/>/g, '').replace(/</g, '');
+      return fetchCollaborators(nextPageUrl, collaborators);
+    }
+    return collaborators;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 module.exports = { Index };
