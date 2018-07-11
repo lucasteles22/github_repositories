5e1fb4861114132b9573ca27bd74326a35ba8762
Lucas Teles <lucasteles22@gmail.com>
2018-07-11 20:59:28 -0300

    Refactor RepositoriesController to use github rest lib

--- controllers/RepositoriesController.js
+++ controllers/RepositoriesController.js
@@ -1,8 +1,13 @@
-const axios = require('axios');
 const GithubGraphQLApi = require('node-github-graphql');
 const github = new GithubGraphQLApi({
   token: process.env.GITHUB_ACCESS_TOKEN,
 });
+
+const githubRestCollaboratorApi = require("../lib/github/rest/collaborator/api");
+
+const githubHost = "https://api.github.com/repos";
+const githubClientId = process.env.GITHUB_CLIENT_ID;
+const githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
 
 const query = `
 query($start: Int, $after: String, $login: String = ""){
@@ -31,10 +36,6 @@ query($start: Int, $after: String, $login: String = ""){
 }
 `;
 
-const githubHost = "https://api.github.com/repos";
-const githubClientId = process.env.GITHUB_CLIENT_ID;
-const githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
-
 const Index = async (_, res) => {
   await fetch(res);
 };
@@ -44,7 +45,7 @@ async function fetch(fn, array = [], start = 100, after = null, login = "thought
     const repositories = req.data.organization.repositories;
     for(let element of repositories.edges) {
       const url = `${ githubHost }${ element.node.resourcePath }/contributors?client_id=${ githubClientId }&client_secret=${ githubClientSecret }`;
-      const collaborators = await fetchCollaborators(url);
+      const collaborators = await githubRestCollaboratorApi.Fetch(url);
       element.collaborators = collaborators;
     }
     array = array.concat(repositories.edges);
@@ -57,46 +58,5 @@ async function fetch(fn, array = [], start = 100, after = null, login = "thought
     }
   });
 };
-
-async function fetchCollaborators(url, collaborators = []) {
-  try {
-    const response = await axios.get(url);
-    collaborators = collaborators.concat(response.data);
-    const link = parseLinkHeader(response.headers.link)
-    let nextPageUrl = undefined;
-
-    if(link && (nextPageUrl = link.next)) {
-      return fetchCollaborators(nextPageUrl, collaborators);
-    }
-
-    return collaborators;
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-function parseLinkHeader(header) {
-  if (!header || header.length === 0) {
-    return null;
-  }
-
-  // Split parts by comma
-  var parts = header.split(',');
-
-  let links = {};
-
-  // Parse each part into a named link
-  for(let i = 0; i < parts.length; i++) {
-    const section = parts[i].split(';');
-    if (section.length !== 2) {
-      return null;
-    }
-    const url = section[0].replace(/<(.*)>/, '$1').trim();
-    const name = section[1].replace(/rel="(.*)"/, '$1').trim();
-    links[name] = url;
-  }
-
-  return links;
-}
 
 module.exports = { Index };
