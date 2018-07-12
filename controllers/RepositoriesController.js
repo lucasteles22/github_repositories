abf3237be7f049c9d559c3bb1669804825ba7113
Lucas Teles <lucasteles22@gmail.com>
2018-07-11 21:12:32 -0300

    Refactor RepositoriesController to use github graphql lib

--- controllers/RepositoriesController.js
+++ controllers/RepositoriesController.js
@@ -1,62 +1,6 @@
-const GithubGraphQLApi = require('node-github-graphql');
-const github = new GithubGraphQLApi({
-  token: process.env.GITHUB_ACCESS_TOKEN,
-});
-
-const githubRestCollaboratorApi = require("../lib/github/rest/collaborator/api");
-
-const githubHost = "https://api.github.com/repos";
-const githubClientId = process.env.GITHUB_CLIENT_ID;
-const githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
-
-const query = `
-query($start: Int, $after: String, $login: String = ""){
-  organization(login: $login){
-    repositories(first: $start, after: $after){
-      pageInfo{
-        hasNextPage,
-        endCursor,
-      },
-      edges{
-        node{
-          stargazers{
-            totalCount,
-          },
-          primaryLanguage{
-            name,
-          },
-          name,
-          url,
-          forkCount,
-          resourcePath,
-        }
-      }
-    }
-  }
-}
-`;
-
+const githubGraphQLCollaboratorApi = require("../lib/github/graphql/repository/api");
 const Index = async (_, res) => {
-  await fetch(res);
-};
-
-async function fetch(fn, array = [], start = 100, after = null, login = "thoughtworks"){
-  github.query(query, { start: start, after: after, login: login }, async (req, res) => {
-    const repositories = req.data.organization.repositories;
-    for(let element of repositories.edges) {
-      const url = `${ githubHost }${ element.node.resourcePath }/contributors?client_id=${ githubClientId }&client_secret=${ githubClientSecret }`;
-      const collaborators = await githubRestCollaboratorApi.Fetch(url);
-      element.collaborators = collaborators;
-    }
-    array = array.concat(repositories.edges);
-
-    if(repositories.pageInfo.hasNextPage) {
-      const endCursor = repositories.pageInfo.endCursor;
-      fetch(fn, array, start, endCursor, login);
-    } else {
-      fn.json(array);
-    }
-  });
+  await githubGraphQLCollaboratorApi.Fetch(res);
 };
 
 module.exports = { Index };
